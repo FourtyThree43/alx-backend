dd84f201b7a34c1e614088762411275096a2a563
FourtyThree43 <shaqmwa@outlook.com>
2023-10-23 20:37:59 +0300

    SOAP is a piece of shit

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -5,7 +5,7 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import List
+from typing import List, Dict, Any
 
 
 class Server:
@@ -35,9 +35,46 @@ class Server:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
+                i: dataset[i]
+                for i in range(len(dataset))
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            pass
+    def get_hyper_index(self,
+                        index: int = None,
+                        page_size: int = 10) -> Dict[str, Any]:
+        """
+            Returns a dictionary with the following key-value pairs:
+            - index: the current start index of the return page.
+                     That is the index of the first item in the current page.
+                     For example if requesting page 3 with page_size 20, and
+                     no data was removed from the dataset,
+                     the current index should be 60.
+            - next_index: the next index to query with.
+                          That should be the index of the first item after
+                          the last item on the current page.
+            - page_size: the current page size
+            - data: the actual page of the dataset
+            """
+        assert type(index) is int and type(page_size) is int
+        assert 0 <= index < len(self.indexed_dataset())
+        assert page_size <= len(self.indexed_dataset())
+
+        data = []
+        next_index = index + page_size
+
+        for i in range(index, next_index):
+            if i not in self.indexed_dataset():
+                next_index += 1
+                index += 1
+                continue
+            data.append(self.indexed_dataset()[i])
+
+        page_info = {
+            'index': index,
+            'data': data,
+            'page_size': page_size,
+            'next_index': next_index,
+        }
+
+        return page_info
